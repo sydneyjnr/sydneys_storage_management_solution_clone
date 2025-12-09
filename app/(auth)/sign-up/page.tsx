c7ba1a2854136b0757c822f21f674aea6a8bd15b
Sydney <Sydneychristopherjnr@gmail.com>
2025-12-10 00:31:40 +0100

    UI for sign-in and sign-up section

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,6 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
-const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+const SignUp = () => <AuthForm type="sign-up"/>
 
 export default SignUp
